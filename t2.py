2f865c32b00170a319c785787595e8e22326c45a
40957039o <126563852+40957039o@users.noreply.github.com>
2023-03-08 15:41:11 +0800

    Update t2.py

--- t2.py
+++ t2.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 df=pd.read_csv("C:/Users/USER/Desktop/學習分析/README.md/少年矯正學校在校受感化教育學生人數按年齡及性別分(統計).csv",encoding='UTF-8') #CSV檔讀取
 # print(df)
-xd = df[['少年犯年齡','統計值']]
+xd = df[['少年犯年齡','統計值']] #需要的資訊
 # print(xd)
-SB = xd.groupby(['少年犯年齡'])['統計值'].sum().reset_index()
+SB = xd.groupby(['少年犯年齡'])['統計值'].sum().reset_index() #數值加總統計
 # print(SB)
 z = SB['統計值'].head(8)/2
 # print(z)
