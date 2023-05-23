f54e7e97d5c4aa6778e43d76a1678eb5c9d04db7
40957039o <126563852+40957039o@users.noreply.github.com>
2023-05-23 22:38:45 +0800

    Add files via upload

---
+++ homework5/README.md
@@ -0,0 +1,6 @@
+#作業5
+
+[程式碼](https://github.com/40957024O-steven/NTNUclass/blob/main/TextSentimentBot/index.js)
+
+[對話照片](https://github.com/40957024O-steven/NTNUclass/blob/main/homework4/%E4%BD%9C%E6%A5%AD4%E7%85%A7%E7%89%87.png)
+
---
+++ homework5/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    //do something
+    $("#thisButton").click(function(){
+        processImage();
+    });
+});
+
+function processImage() {
+    
+    //確認區域與所選擇的相同或使用客製化端點網址
+    var url = "https://eastus.api.cognitive.microsoft.com/";
+    var uriBase = url + "vision/v2.1/analyze";
+    
+    var params = {
+        "visualFeatures": "Adult",
+        "details": "",
+        "language": "en",
+    };
+    //顯示分析的圖片
+    var sourceImageUrl = document.getElementById("inputImage").value;
+    document.querySelector("#sourceImage").src = sourceImageUrl;
+    //送出分析
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+        // Request header
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+        type: "POST",
+        // Request body
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+    .done(function(data) {
+        //顯示JSON內容
+        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        $("#picDescription").empty();
+        if(data.adult.isAdultContent === false && data.adult.isRacyContent === false && data.adult.isGoryContent === false){
+        $("#picDescription").append('這張照片是您孩子可以觀看的'+"<br>")    
+        }
+        else{
+        $("#picDescription").append('這張照片不建議您給您孩子觀看'+"<br>")    
+        }
+        ;
+        // $("#picDescription").append("這裡有"+data.faces.length+"個人");
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        //丟出錯誤訊息
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
+        alert(errorString);
+    });
+};
